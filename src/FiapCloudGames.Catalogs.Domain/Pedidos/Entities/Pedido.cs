b43c407fa18925591b5545b5760cde40785c045e
Giovanne Barbosa <giovanne.bbarbosa@gmail.com>
2026-06-22 20:23:06 -0300

    Cria domain de pedido

---
+++ src/FiapCloudGames.Catalogs.Domain/Pedidos/Entities/Pedido.cs
@@ -0,0 +1,9 @@
+﻿namespace FiapCloudGames.Catalogs.Domain.Pedidos.Entities
+{
+    public class Pedido
+    {
+        public string NomeUsuario { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public Guid IdJogo { get; set; }
+    }
+}
